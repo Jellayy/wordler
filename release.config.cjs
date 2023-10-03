62494929ac9692ca9427d0d1c71a2f7580cb07a4
Austin Huffman <jellayy@mbp.jellayy.com>
2023-10-03 16:42:33 -0700

    ci: release.config.cjs formatting

--- release.config.cjs
+++ release.config.cjs
@@ -6,16 +6,22 @@ module.exports = {
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
-        '@semantic-release/git', {
-            "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-        },
-        '@semantic-release/github', {
-            "assets": [
-                {"path": "release/wordler-windows-amd64.exe", "label": "Windows 64bit"},
-                {"path": "release/wordler-darwin-amd64", "label": "MacOS 64bit Intel"},
-                {"path": "release/wordler-darwin-arm64", "label": "MacOS 64bit Apple Silion"},
-                {"path": "release/wordler-linux-amd64", "label": "Linux 64bit"}
-            ]
-        }
+        [
+            '@semantic-release/git',
+            {
+                "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+            }
+        ],
+        [
+            '@semantic-release/github',
+            {
+                "assets": [
+                    {"path": "release/wordler-windows-amd64.exe", "label": "Windows 64bit"},
+                    {"path": "release/wordler-darwin-amd64", "label": "MacOS 64bit Intel"},
+                    {"path": "release/wordler-darwin-arm64", "label": "MacOS 64bit Apple Silion"},
+                    {"path": "release/wordler-linux-amd64", "label": "Linux 64bit"}
+                ]
+            }
+        ]
     ]
 };
