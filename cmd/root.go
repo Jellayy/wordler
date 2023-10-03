5c3a16fe646a9c788ad5a6c2a68a38841738cb04
Austin Huffman <jellayy@mbp.jellayy.com>
2023-10-03 13:25:49 -0700

    ci: release & build

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,3 @@
-/*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
@@ -21,9 +18,6 @@ var rootCmd = &cobra.Command{
 	Long: colorBlue + "                          ____\n _      ______  _________/ / /__  _____\n| | /| / / __ \\/ ___/ __  / / _ \\/ ___/\n| |/ |/ / /_/ / /  / /_/ / /  __/ /\n|__/|__/\\____/_/   \\__,_/_/\\___/_/\n\n" + colorReset +
 		`The wordler CLI provides a number of helpful tools for the New York Times' hit game: Wordle.
 Including the ability to play the game!`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -35,14 +29,4 @@ func Execute() {
 	}
 }
 
-func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.wordler.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
+func init() {}
